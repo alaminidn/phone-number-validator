54be15403747d3ac130dc7a3b0e470ba4be0bc44
mrhrifat <mrhrifat383759@gmail.com>
2021-12-11 19:07:17 +0600

    Add Root component

---
+++ src/components/Root.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Root extends Component {
+    render() {
+        return (
+            <div class='container'>
+                <h6>Bismillahir Rahmanir Rahim</h6>
+            </div>
+        )
+    }
+}
+
+export default Root
