063f8a38777c865b2c448628d7a97db0e1c9a717
mrhrifat <mrhrifat383759@gmail.com>
2021-12-15 22:28:56 +0600

    Update component style

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from 'react'
+import '../style/style.css'
 
 class Root extends Component {
     render() {
         return (
             <div class='container'>
-                <div className="root-container">
-                    <div className="main-body">
+                <div className="root-container d-flex justify-content-center">
+                    <div className="main-body w-md-50 bg-light p-5 rounded">
                         <form>
-                            <div className="form-group">
-                                <label htmlFor='phone'>Phone Number</label>
-                                <input type='text' className='form-control' id='phone' placeholder='Enter Phone Number' />
+                            <div className='form-group'>
+                                <label htmlFor='phone'>Enter phone number</label>
+                                <input
+                                    type="text"
+                                    name="phone"
+                                    id="phone"
+                                    className='form-control'
+                                    // value={natural}
+                                    onChange={this.handleChange}
+                                    onKeyPress={this.handleKeyPress}
+                                    placeholder='Ex: +8801XX-XXXXXXX'
+                                />
                             </div>
-                            <div className="form-group">
-                                <input type="submit" value="Check Validity" />
+                            <div className='form-group my-3'>
+                                <input
+                                    type="button"
+                                    className='btn btn-sm btn-success'
+                                    // onClick={this.handleSubmit}
+                                    value="Check Validity"
+                                />
                             </div>
                         </form>
                     </div>
